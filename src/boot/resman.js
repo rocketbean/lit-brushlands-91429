0f5175ac50530f3c7fa28669779ee6905d61336b
nikko <buzzrocketbean@gmail.com>
2021-10-16 19:28:18 +0800

    herd build

--- src/boot/resman.js
+++ src/boot/resman.js
@@ -2,9 +2,10 @@
 
 window.$serve = 'local'; // dev
 if($serve == "local") {
-  window.$baseApi = "http://localhost:3000/api/"
-  window.$baseUrl = "http://localhost:3000/resman/"
-  window.$peerhost = "ws://localhost:3000/"
+
+  window.$baseApi = "https://cryptic-woodland-38791.herokuapp.com/api/"
+  window.$baseUrl = "https://cryptic-woodland-38791.herokuapp.com/resman/"
+  window.$peerhost = "wss://cryptic-woodland-38791.herokuapp.com/"
 } else {
   // window.$baseUrl = "https://castmonkeys.com:3000/" // "https://192.168.254.133:3000/"
   // window.$peerhost = "wss://castmonkeys.com:3000/" //"wss://192.168.254.133:3000"
